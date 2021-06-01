1db40869be459acea5cb0aecb7e0209b67cd8c9d
ZohaibX <zohaibbutt045@gmail.com>
2021-06-01 20:51:55 +0500

    Updates

--- npm/src/middlewares/current-user.ts
+++ npm/src/middlewares/current-user.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import { AccountTypes } from '../events/project-1/types/account-types';
 
 interface UserPayload {
   id: string;
   email: string;
+  accountType:  AccountTypes
 }
 
 declare global {
