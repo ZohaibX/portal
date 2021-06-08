fa3243d005feb91715c8a3f73776c6db02e39737
ZohaibX <zohaibbutt045@gmail.com>
2021-06-09 03:31:15 +0500

    Updates

--- npm/src/events/project-1/course-create-event.ts
+++ npm/src/events/project-1/course-create-event.ts
@@ -3,6 +3,7 @@ import { Accounts } from './types/subjects';
 export interface CourseCreateEvent {
   subject: Accounts.CourseCreated; 
   data: {
+    user_id: string, 
     _id: string,
     course_name: string ,
     course_code: number 
